15f440d15556b74abaa58e8a9c952294a584f9a1
Ten Mutunhire <tmutunhire@gmail.com>
2015-04-16 12:41:40 +0000

    complete app

--- js/application.js
+++ js/application.js
@@ -1,6 +1,8 @@
 window.Todos = Ember.Application.create();
 
-Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
+Todos.ApplicationAdapter = DS.LSAdapter.extend({
+    namespace: 'todos-emberjs'
+});
 
 function createTodo() {
     alert("yeah nigga");
